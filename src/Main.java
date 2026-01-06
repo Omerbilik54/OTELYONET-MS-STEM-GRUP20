ab89bc634652350f1b150e5dc0e32da3697ecc96
Ömercan <bilikomer89@gmail.com>
2026-01-06 13:38:04 +0300

    Kod output

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main{
+	public static void main(String[] args){
+	//FR5.1 - kullanici islemler menu uzerinde secebilmelidir
+		MenuController app = new MenuController();
+		app.calistir();
+
+
+
+
+        }
+	}
+
